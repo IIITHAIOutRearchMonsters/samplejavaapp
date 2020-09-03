bbb7b20b826a37b96b2919f16bcfe3435c158c83
IIITHAI&MLOutRearchProfessionals <44643853+IIITHAIOutRearchMonsters@users.noreply.github.com>
2020-09-03 18:06:26 +0800

    Update CaseInsensitiveComparator.java

--- src/main/java/com/devopsdemo/utilities/CaseInsensitiveComparator.java
+++ src/main/java/com/devopsdemo/utilities/CaseInsensitiveComparator.java
@@ -93,6 +93,7 @@ public class CaseInsensitiveComparator extends GenericComparator {
                 } else if (obj.equals(DATATYPE_DOUBLE)) {
                         acutal = ((Double) v1).compareTo((Double) v2) * determinePosition();
                 }
+                
                 return acutal;
         }
 
